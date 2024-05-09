6bd08087744293b196a9e4bf7d0bbe3b68616ae3
DidisGamos <didisgamos@gmail.com>
2024-05-09 10:49:26 +0300

    okey

---
+++ app_number.js
@@ -0,0 +1,30 @@
+let num1 = 0;
+const first = document.getElementById('num1');
+const interval1=setInterval(() => {
+    num1++;
+    first.textContent = num1;
+    if (num1 > 499) {
+        clearInterval(interval1);
+    }
+}, 100);
+
+let num2 = 0;
+const second = document.getElementById('num2');
+function numto17148() {
+    num2++;
+    second.textContent = num2;
+    if (num2 < 17148) {
+        setTimeout(numto17148, 1);
+    }
+}
+numto17148();
+
+let num3 = 0;
+const third = document.getElementById('num3');
+const interval2=setInterval(() => {
+    num3++;
+    third.textContent = num3;
+    if (num3 > 1499) {
+        clearInterval(interval2);
+    }
+}, 10);
